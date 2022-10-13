c6b726e140761647c5e405e96a56e725d3e9cff0
Ahmad Syauqi <syauqiahmd@Ahmads-MacBook-Pro.local>
2022-10-13 13:49:41 +0800

    public view - register,login,home

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,12 @@
 class ControllerHome{
   static home(req,res){
-    res.render('index')
+    res.render('public/index')
+  }
+  static login(req,res){
+    res.render('public/login')
+  }
+  static register(req,res){
+    res.render('public/register')
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const ControllerHome = require('../controllers/index')
 const router = express.Router()
 
 router.get('/', ControllerHome.home)
+router.get('/login', ControllerHome.login)
+router.get('/register', ControllerHome.register)
 
 module.exports = router
