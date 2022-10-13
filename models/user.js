f6c5a612c0f39fa4bb3270652b3a8b82a2f94254
Ahmad Syauqi <syauqiahmd@Ahmads-MacBook-Pro.local>
2022-10-14 04:52:46 +0800

    hotfix

--- models/user.js
+++ models/user.js
@@ -27,6 +27,7 @@ module.exports = (sequelize, DataTypes) => {
     email: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
       validate: {
         notEmpty: {
           msg: 'E-mail must be filled'
